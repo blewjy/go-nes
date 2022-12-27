70055389da0254dc56d5174e8552710b0d49cda1
Bryan Lew <blewjy@gmail.com>
2022-12-27 13:11:39 +0800

    feat: implement basic ppu register behaviour

--- nes/ppu.go
+++ nes/ppu.go
@@ -1,3 +1,46 @@
+/*
+Info from javidx9's NES emulator video: https://www.youtube.com/watch?v=-THeUXqR3zY
+
+PPU Registers:
+
+0x2000 - PPUCTRL
+Responsible for configuring the PPU to render in different ways
+
+0x2001 - PPUMASK
+Decides if backgrounds/sprites are being drawn, and what's happening at the edges of the screen
+
+0x2002 - PPUSTATUS
+Tells us when it is safe to render
+
+0x2005 - PPUSCROLL
+Scrolling information
+
+0x2006 - PPUADDR
+Allows CPU to read and write to PPU's address?
+
+0x2007 - PPUDATA
+Allows CPU to read and write to PPU's data?
+
+
+How PPU renders a frame?
+
+Each scanline can be thought of as a row of pixels horizontally across the screen.
+Each pixel in that scanline can be thought of as a cycle, and there are 341 cycles per scanline.
+However, the NES screen is only 256 pixels wide and 240 pixels high.
+Therefore, each scanline will exceed the 256 width of the screen.
+
+In the vertical axis, there are a total of 261 scanlines, which means that it exceeds the height of the screen by 21 lines.
+This 21 lines is known as the vertical blank period.
+The game needs to know when this period starts, because the CPU uses this area to do some processing to the PPU (because we can't see it on the screen).
+It is typically during this period that CPU does some setup on the PPU to prepare for the next frame.
+
+So the vertical blank bit in the PPUSTATUS tells us if we are in screen space (0) or blank space (1).
+
+At the point of hitting the vertical blank, we also may emit an NMI signal to the CPU.
+Whether or not this is emitted depends on the flag of the PPUCTRL register.
+
+*/
+
 package nes
 
 import (
@@ -12,6 +55,24 @@ type PPU struct {
 	tablePattern [2][4096]uint8
 	tableName    [2][1024]uint8
 	tablePalette [32]uint8
+
+	// PPU registers
+	ppuCtrl   uint8 // 0x2000
+	ppuMask   uint8 // 0x2001
+	ppuStatus uint8 // 0x2002
+	oamAddr   uint8 // 0x2003
+	oamData   uint8 // 0x2004
+	ppuScroll uint8 // 0x2005
+	ppuAddr   uint8 // 0x2006
+	ppuData   uint8 // 0x2007
+
+	// ???
+	oamDma uint8 // 0x4014
+
+	// PPU helper variables
+	addressLatch  uint8
+	ppuDataBuffer uint8
+	ppuAddress    uint16
 
 	// ???
 	scanline      int
@@ -46,11 +107,32 @@ func (p *PPU) CpuRead(addr uint16) uint8 {
 	case 0x0000: // Control
 	case 0x0001: // Mask
 	case 0x0002: // Status
+		p.ppuStatus |= 0x80 // temporary hack
+
+		data = (p.ppuStatus & 0xE0) | (p.ppuDataBuffer & 0x1F) // quirk
+
+		// reset vertical blank
+		p.ppuStatus &= 0x3F
+
+		// reset address latch
+		p.addressLatch = 0
+
 	case 0x0003: // OAM Address
 	case 0x0004: // OAM Data
 	case 0x0005: // Scroll
 	case 0x0006: // PPU Address
 	case 0x0007: // PPU Data
+		data = p.ppuDataBuffer
+		p.ppuDataBuffer = p.PpuRead(p.ppuAddress)
+
+		// for palette data, don't need to buffer one cycle
+		if p.ppuAddress > 0x3F00 {
+			data = p.ppuDataBuffer
+		}
+
+		// auto-increment
+		p.ppuAddress++
+
 	}
 
 	return data
@@ -59,13 +141,27 @@ func (p *PPU) CpuRead(addr uint16) uint8 {
 func (p *PPU) CpuWrite(addr uint16, data uint8) {
 	switch addr {
 	case 0x0000: // Control
+		p.ppuCtrl = data
 	case 0x0001: // Mask
+		p.ppuMask = data
 	case 0x0002: // Status
+		// you can't write to this register
 	case 0x0003: // OAM Address
 	case 0x0004: // OAM Data
 	case 0x0005: // Scroll
 	case 0x0006: // PPU Address
+		if p.addressLatch == 0 {
+			p.ppuAddress = p.ppuAddress&0x00FF | (uint16(data) << 8)
+			p.addressLatch = 1
+		} else {
+			p.ppuAddress = p.ppuAddress&0xFF00 | uint16(data)
+			p.addressLatch = 0
+		}
 	case 0x0007: // PPU Data
+		p.PpuWrite(p.ppuAddress, data)
+
+		// auto-increment
+		p.ppuAddress++
 	}
 }
 
@@ -162,7 +258,6 @@ func (p *PPU) GetPatternTableDisplay(tableIndex, paletteId int) [128][128]color.
 			paletteByteOffset := 0x3F00 + uint16(paletteId)<<2 + uint16(pixelBits)
 
 			colorIndex := p.PpuRead(paletteByteOffset)
-			colorIndex = pixelBits * 21
 
 			display[i][j] = p.colorPalette[colorIndex]
 		}
